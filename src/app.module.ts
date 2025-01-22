ec8fdc48e355e636ba39de0c2ce8612d32a1b451
JS-JeevaSaravanan <jeevatamil2000@gmail.com>
2025-01-22 19:14:39 +0530

    ✨ add(module): movie2 module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { HttpModule } from '@nestjs/axios';
 import { CalculatorService } from './calculator/calculator.service';
 import { SumService } from './calculator/sum.service';
 import { MovieModule } from './movie/movie.module';
+import { Movie2Module } from './movie2/movie2.module';
 
 @Module({
-  imports: [HttpModule, MovieModule],
+  imports: [HttpModule, MovieModule, Movie2Module],
   controllers: [AppController],
   providers: [AppService, CalculatorService, SumService],
 })
